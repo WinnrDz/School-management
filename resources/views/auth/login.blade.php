59aa5611f706a7292e93ef0c3e72e78c8af3c7f3
rabah <rabawinnr@gmail.com>
2025-09-03 17:08:39 +0100

    redesigned login and register (still not fully functional)

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,50 +1,44 @@
-@extends('layout')
-@section('content')
-<x-guest-layout>
-    <!-- Session Status -->
-    <x-auth-session-status class="mb-4" :status="session('status')" />
+<!DOCTYPE html>
+<html lang="en">
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="{{ asset('css/login.css') }}">
+</head>
 
-        <!-- Email Address -->
-        <div>
-            <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus autocomplete="username" />
-            <x-input-error :messages="$errors->get('email')" class="mt-2" />
-        </div>
 
-        <!-- Password -->
-        <div class="mt-4">
-            <x-input-label for="password" :value="__('Password')" />
 
-            <x-text-input id="password" class="block mt-1 w-full"
-                type="password"
-                name="password"
-                required autocomplete="current-password" />
+<body>
+  <!-- Session Status -->
+  <x-auth-session-status class="mb-4" :status="session('status')" />
+  
+  <form method="POST" action="{{ route('login') }}">
+    @csrf
+    
+    <h1>Welcome back.</h1>
+    <p>Please login to continue.</p>
+    <label>Email Adress.</label>
+    <input type="email" name="email" placeholder="example@mail.com" id="email" required>
+    <label>Password.</label>
+    <input type="password" placeholder="Enter Password." id="password"
+      name="password"
+      required autocomplete="current-password">
+    <p>It must be a combination of minimum 8 letters, numbers, and symbols.</p>
+    <div class="options">
+      <div class="checkbox">
+        <input type="checkbox" id="remember_me" name="remember">
+        <p>Remember me</p>
+      </div>
+      <span>forgot password ?</span>
+    </div>
+    <button>{{ __('Log in') }}</button>
 
-            <x-input-error :messages="$errors->get('password')" class="mt-2" />
-        </div>
+    <a href="{{ route('register') }} " class="reg-btn">
+      Register
+    </a>
+  </form>
+</body>
 
-        <!-- Remember Me -->
-        <div class="block mt-4">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded dark:bg-gray-900 border-gray-300 dark:border-gray-700 text-indigo-600 shadow-sm focus:ring-indigo-500 dark:focus:ring-indigo-600 dark:focus:ring-offset-gray-800" name="remember">
-                <span class="ms-2 text-sm text-gray-600 dark:text-gray-400">{{ __('Remember me') }}</span>
-            </label>
-        </div>
-
-        <div class="flex items-center justify-end mt-4">
-            @if (Route::has('password.request'))
-            <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800" href="{{ route('password.request') }}">
-                {{ __('Forgot your password?') }}
-            </a>
-            @endif
-
-            <x-primary-button class="ms-3">
-                {{ __('Log in') }}
-            </x-primary-button>
-        </div>
-    </form>
-</x-guest-layout>
-@endsection
+</html>
--- routes/web.php
+++ routes/web.php
@@ -23,13 +23,14 @@ use App\Http\Controllers\GradeController;
 
 
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
 
-    Route::get('/', [SchoolClassController::class, "index"])->name("schoolClasses.index");
+    Route::get('/', function () {
+        return view('dashboard');
+    })->middleware(['auth', 'verified'])->name('dashboard');
+
+
 
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
