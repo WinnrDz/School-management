5ebefa1ad6a3f01a57d9978ef3367b8561daf19a
rabah <rabawinnr@gmail.com>
2025-09-05 16:56:27 +0100

    worked on the dashboard design/ combined all css in one file

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,31 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layout')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="{{ asset('css/dashboard.css') }}">
-</head>
-
-<body>
-    <nav>
-        <img src="{{ asset('images/School-management Logo.png') }}" alt="logo" class="logo">
-        <div class="btns-grps">
-            <img src="{{ asset('images/user.png') }}" alt="profile" class="profile-img">
-            <img src="{{ asset('images/settings.png') }}" alt="settings" class="settings-img">
-            <img src="{{ asset('images/bell.png') }}" alt="bell" class="bell-img">
-        </div>
-        <input type="search" placeholder="Search for..." />
-        <div class="menu">
-            <div class="classes"><img src="{{ asset('images/class.png') }}" alt="class" class="user-class-img"><span>Classes</span></div>
-            <div class="students"><img src="{{ asset('images/student.png') }}" alt="students" class="student-img"><span>Students</span></div>
-            <div class="attendances"><img src="{{ asset('images/person-circle-check.png') }}" alt="person-circle-checks" class="person-circle-check-img"><span>Attendances</span></div>
-            <div class="teachers"><img src="{{ asset('images/lesson-class.png') }}" alt="teacher" class="teacher-img"><span>Teachers</span></div>
-            <div class="subjects"><img src="{{ asset('images/books.png') }}" alt="subject" class="subject-img"><span>Subjects</span></div>
-            <div class="grades"><img src="{{ asset('images/test.png') }}" alt="grades" class="grade-img"><span>Grades</span></div>
-        </div>
-    </nav>
+@section('content')
     <main>
         <h1> {{ __('Dashboard') }}</h1>
         <div class="dashboard-tabs">
@@ -69,8 +44,4 @@
         </div>
         
 
-    </main>
-
-</body>
-
-</html>
+@endsection
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -4,25 +4,27 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Classes</title>
-    <link rel="stylesheet" href="{{ asset('css/style.css')}}">
+    <title>Document</title>
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
 </head>
 
 <body>
-    <nav class="navbar">
-        <div class="logo">School Management</div>
-        <ul class="nav-links">
-            <li><a href="{{ route('schoolClasses.index') }}">Classes</a></li>
-            <li><a href="{{ route('students.index') }}">Students</a></li>
-            <li><a href="{{ route('attendances.index') }}">attendances</a></li>
-            <li><a href="{{ route('teachers.index') }}">Teachers</a></li>
-            <li><a href="{{ route('subjects.index') }}">Subjects</a></li>
-            <li><a href="{{ route('grades.index') }}">Grades</a></li>
-            <li><a href="#">About</a></li>
-            <li><a href="{{ route('login') }}">login</a></li>
-            <li><a href="{{ route('register') }}">register</a></li>
-            
-        </ul>
+    <nav>
+        <img src="{{ asset('images/School-management Logo.png') }}" onclick="window.location.href=`{{ route('dashboard') }}`" alt="logo" class="logo">
+        <div class="btns-grps">
+            <img src="{{ asset('images/user.png') }}" alt="profile" class="profile-img">
+            <img src="{{ asset('images/settings.png') }}" alt="settings" class="settings-img">
+            <img src="{{ asset('images/bell.png') }}" alt="bell" class="bell-img">
+        </div>
+        <input type="search" placeholder="Search for..." />
+        <div class="menu">
+            <div onclick="window.location.href=`{{ route('schoolClasses.index') }}`"  class="classes"><img src="{{ asset('images/class.png') }}" alt="class" class="user-class-img"><span>Classes</span></div>
+            <div onclick="window.location.href=`{{ route('students.index') }}`"  class="students"><img src="{{ asset('images/student.png') }}" alt="students" class="student-img"><span>Students</span></div>
+            <div onclick="window.location.href=`{{ route('attendances.index') }}`"  class="attendances"><img src="{{ asset('images/person-circle-check.png') }}" alt="person-circle-checks" class="person-circle-check-img"><span>Attendances</span></div>
+            <div onclick="window.location.href=`{{ route('teachers.index') }}`"  class="teachers"><img src="{{ asset('images/lesson-class.png') }}" alt="teacher" class="teacher-img"><span>Teachers</span></div>
+            <div onclick="window.location.href=`{{ route('subjects.index') }}`"  class="subjects"><img src="{{ asset('images/books.png') }}" alt="subject" class="subject-img"><span>Subjects</span></div>
+            <div onclick="window.location.href=`{{ route('grades.index') }}`"  class="grades"><img src="{{ asset('images/test.png') }}" alt="grades" class="grade-img"><span>Grades</span></div>
+        </div>
     </nav>
 
     <main class="main-content">
